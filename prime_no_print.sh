f2ab4d2d6e499e7bf527ff5ad0c8c89edce8679b
10011999 <motewaraditya77@gmail.com>
2022-05-26 12:11:23 +0530

    added: functions question 3_a. question

---
+++ prime_no_print.sh
@@ -0,0 +1,16 @@
+#read -p "enter a number: "
+function number()
+{
+num=$1
+for(( i=2; i<=num/2; i++ ))
+do
+if (( $((num%i)) == 0 ))
+then
+echo "num is not a prime no"
+break
+fi
+done
+}
+echo "num is a prime number"
+number 11
+number 12
